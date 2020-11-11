14b0f50652544f6aecee31d19cec78eafa7ea332
RachmanForniandi <rachmanforniandi@gmail.com>
2020-11-11 08:04:58 +0700

    implementing databinding clickListener to detail page done.

--- app/src/main/java/com/example/dogspediakotlin/adapters/DogsListAdapter.kt
+++ app/src/main/java/com/example/dogspediakotlin/adapters/DogsListAdapter.kt
@@ -1,15 +1,18 @@
 package com.example.dogspediakotlin.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dogspediakotlin.R
 import com.example.dogspediakotlin.databinding.ItemDataDogsBinding
 import com.example.dogspediakotlin.models.DogBreeds
+import com.example.dogspediakotlin.views.fragments.ListFragmentDirections
 import kotlinx.android.synthetic.main.item_data_dogs.view.*
 
-class DogsListAdapter(val dogsList: ArrayList<DogBreeds>): RecyclerView.Adapter<DogsListAdapter.DogsListHolder>() {
+class DogsListAdapter(val dogsList: ArrayList<DogBreeds>): RecyclerView.Adapter<DogsListAdapter.DogsListHolder>(),DogClickListener {
 
     fun updateDogList(newDogList:List<DogBreeds>){
         dogsList.clear()
@@ -26,8 +29,21 @@ class DogsListAdapter(val dogsList: ArrayList<DogBreeds>): RecyclerView.Adapter<
     override fun getItemCount(): Int = dogsList.size
 
     override fun onBindViewHolder(holder: DogsListHolder, position: Int) {
-       holder.view.dogs = dogsList[position]
+        holder.view.dogs = dogsList[position]
+        holder.view.listener = this
+    }
+
+
+    override fun onDogClicked(v: View) {
+        val uuid = v.dog_Id.text.toString().toInt()
+        val action = ListFragmentDirections.actionDetailFragment()
+        action.dogUuid = uuid
+        Navigation.findNavController(v).navigate(action)
     }
 
     class DogsListHolder ( var view: ItemDataDogsBinding): RecyclerView.ViewHolder(view.root)
+
+
+
+
 }
